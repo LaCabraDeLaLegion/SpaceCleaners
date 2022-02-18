5b1e96a8f438cd27a7a47f3ecad5ec0b76c320b9
Tania <tanirome@ucm.es>
2022-02-18 21:26:18 +0100

    Mapa cutre

--- proyecto_phaser/src/init.js
+++ proyecto_phaser/src/init.js
@@ -1,12 +1,13 @@
 import Level from "./level.js";
 import Menu from "./menu.js";
+import Map from "./map.js";
 
 var config = {
   type: Phaser.CANVAS,
   width: 920,
   height: 500,
   parent: "container",
-  scene: [Menu, Level],
+  scene: [Menu, Map, Level],
   render: {pixelArt: true},
   physics: {
     default: "arcade",
---
+++ proyecto_phaser/src/map.js
@@ -0,0 +1,29 @@
+export default class Map extends Phaser.Scene {
+    constructor() {
+      super({ key: "map" });
+    }
+  
+    preload() {
+      this.load.setPath("assets/sprites/Map/");
+      this.load.image("background_map", "background_map.png");
+      this.load.image("grey_planet", "Baren.png");
+      this.load.image("ice_planet", "Ice.png");
+      this.load.image("lava_planet", "Lava.png");
+      this.load.image("earth_planet", "Terran.png");
+    }
+  
+    create() {
+        this.add.image(450, 250, "background_map").setDepth(1).setScale(2).setRotation(300);
+        let grey = this.add.image(350, 150, "grey_planet").setDepth(1).setScale(1.5);
+        let ice = this.add.image(350, 350, "ice_planet").setDepth(1);
+        let lava = this.add.image(650, 200, "lava_planet").setDepth(1).setScale(2);
+        let earth = this.add.image(150, 250, "earth_planet").setDepth(1);
+
+        earth.setInteractive();
+        earth.on("pointerup", () => {
+            this.scene.start("level");
+        });
+    }
+  
+  }
+  
